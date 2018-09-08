4137dee7edb92accbaeea88a9c33874aa0ba6573
Patryk Przekwas <patryk@clusterone.com>
2018-09-08 13:59:40 +0200

    WordCount example fixed

--- src/main/scala/wordcnt/WordCount.scala
+++ src/main/scala/wordcnt/WordCount.scala
@@ -2,23 +2,18 @@ package wordcnt
 
 import org.apache.flink.api.scala._
 
-object WordCount {
-  def main(args: Array[String]): Unit = {
+object WordCount extends App {
+  val env = ExecutionEnvironment.getExecutionEnvironment
+  val text = env.readTextFile("/tmp/in.txt")
 
-    val env = ExecutionEnvironment.getExecutionEnvironment
-    val text = env.readTextFile("/tmp/in.txt")
+  val counts = text
+    .flatMap { _.toLowerCase.split("\\W+") filter { _.nonEmpty } }
+    .map { (_, 1) }
+    .groupBy(0)
+    .sum(1)
 
-    val counts = text
-      .flatMap {
-        _.toLowerCase.split("\\W+") filter { _.nonEmpty }
-      }
-      .map { (_, 1) }
-      .groupBy(0)
-      .sum(1)
+  counts.writeAsCsv("/tmp/out",
+    "\n", " ")
 
-    counts.writeAsCsv("/tmp/out",
-      "\n", " ")
-
-    env.execute()
-  }
+  env.execute()
 }
